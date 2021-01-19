5e469fbe7d6412a055455c59672af92676741cf2
Cezar Augusto <boss@cezaraugusto.net>
2021-01-19 20:52:47 -0300

    Support history override;

--- steps/generate-manifest-entries/scriptFromHtmlEntry/index.js
+++ steps/generate-manifest-entries/scriptFromHtmlEntry/index.js
@@ -1,6 +1,6 @@
 const bookmarksOverrideScriptEntry = require('./bookmarksOverrideScript')
 const devtoolsScriptEntry = require('./devtoolsScript')
-// const historyOverrideScriptEntry = require('./historyOverrideScript')
+const historyOverrideScriptEntry = require('./historyOverrideScript')
 // const newtabOverrideScriptEntry = require('./newtabOverrideScript')
 // const optionsScriptEntry = require('./optionsScript')
 // const popupScriptEntry = require('./popupScript')
@@ -9,7 +9,7 @@ module.exports = async function (manifestPath) {
   return [
     ...await bookmarksOverrideScriptEntry(manifestPath),
     ...await devtoolsScriptEntry(manifestPath),
-    // ...await historyOverrideScriptEntry(manifestPath),
+    ...await historyOverrideScriptEntry(manifestPath),
     // ...await newtabOverrideScriptEntry(manifestPath),
     // ...await optionsScriptEntry(manifestPath),
     // ...await popupScriptEntry(manifestPath)
