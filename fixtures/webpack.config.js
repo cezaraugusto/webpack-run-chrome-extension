8e5ff15d8fbb3873c3215b124ae542474a2fef62
Cezar Augusto <boss@cezaraugusto.net>
2021-01-19 00:00:21 -0300

    Prepare for watch work

--- fixtures/webpack.config.js
+++ fixtures/webpack.config.js
@@ -2,9 +2,8 @@ const os = require('os')
 const path = require('path')
 
 const OpenChromeExtension = require('../module')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const CopyPlugin = require('copy-webpack-plugin')
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   cache: {
@@ -23,17 +22,7 @@ module.exports = {
   },
   mode: 'development',
   watch: true,
-  devServer: {
-    hot: true
-    // TODO: check if it works without this
-    // contentBase: path.resolve(__dirname, './demo-extension'),
-    // watchContentBase: true
-  },
   entry: {
-    // Devtools: [
-    //   path.resolve(__dirname, './demo-extension/devtools/sidebar.js'),
-    //   path.resolve(__dirname, './demo-extension/devtools/devtools.js'),
-    // ],
     custom: [
       path.resolve(__dirname, './demo-extension/custom/custom1.js')
     ]
@@ -45,53 +34,23 @@ module.exports = {
     new OpenChromeExtension({
       extensionPath: path.resolve(__dirname, './demo-extension')
     }),
-    // new MiniCssExtractPlugin(),
-    // new HtmlWebpackPlugin({
-    //   filename: './dist/custom/custom.html',
-    //   chunks: ['custom'],
-    //   template: path.resolve(__dirname, './demo-extension/custom/custom.html'),
-    //   inject: false
-    // }),
+    new HtmlWebpackPlugin({
+      filename: './dist/custom/custom.html',
+      chunks: ['custom'],
+      template: path.resolve(__dirname, './demo-extension/custom/custom.html'),
+      inject: false
+    }),
     // Allows watching changes in CSS files for content scripts
     // and the public path.
     // You can get the original source filename from Asset Objects.
     // https://webpack.js.org/api/stats/#asset-objects
-    // new CopyPlugin({
-    //   patterns: [
-    //     {
-    //       from: path.resolve(__dirname, './demo-extension/manifest.json'),
-    //       to: './dist'
-    //     },
-    //     // TODO: should this be handled by public path?
-    //     {
-    //       from: path.resolve(__dirname, './demo-extension/public'),
-    //       to: './dist/public'
-    //     },
-    //     {
-    //       from: path.resolve(__dirname, './demo-extension/content/content1.css'),
-    //       to: './dist/content'
-    //     },
-    //     {
-    //       from: path.resolve(__dirname, './demo-extension/content/content2.css'),
-    //       to: './dist/content'
-    //     },
-    //     {
-    //       from: path.resolve(__dirname, './demo-extension/custom/custom.css'),
-    //       to: './dist/custom'
-    //     },
-    //     {
-    //       from: path.resolve(__dirname, './demo-extension/devtools/devtools.css'),
-    //       to: './dist/devtools'
-    //     }
-    //   ]
-    // })
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './demo-extension/custom/custom.css'),
+          to: './dist/custom'
+        }
+      ]
+    })
   ]
-  // Module: {
-  // rules: [
-  //   {
-  //     test: /\.css$/i,
-  //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
-  //   },
-  // ]
-  // }
 }
